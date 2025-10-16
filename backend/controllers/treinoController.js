3b1f902278710c571f55024f742feeb957a79a22
Marcus_Oliveira <151086480+TIMarcusOliveira@users.noreply.github.com>
2025-10-16 13:32:14 -0300

    progress

--- backend/controllers/treinoController.js
+++ backend/controllers/treinoController.js
@@ -1,26 +1,26 @@
 const LoggerMessages = require("../loggerMessages");
 const { Treino, Funcionario, Exercicio } = require("../models");
 
-const treinoController = {
-  async listar(req, res) {
-    try {
-      const treinos = await Treino.findAll({
-        include: [
-          { model: Funcionario, attributes: ["fu_id", "fu_nome"] },
-          {
-            model: Exercicio,
-            through: { attributes: [] },
-            attributes: ["ex_id", "ex_nome", "ex_grupo_muscular"]
-          }
-        ],
-      });
+const listarTreinos = async (req, res) => {
+  try {
+    const treinos = await Treino.findAll({
+      // include: [
+      //   { model: Funcionario, attributes: ["fu_id", "fu_nome"] },
+      //   {
+      //     model: Exercicio,
+      //     through: { attributes: [] },
+      //     attributes: ["ex_id", "ex_nome", "ex_grupo_muscular"],
+      //   },
+      // ],
+    });
 
-      return res.status(200).json(treinos);
-    } catch (error) {
-      console.error("Erro ao buscar treinos:", error); // colocar logger messages
-      return res.status(500).json({ error: "Erro ao buscar treinos" }); // colocar logger messages
-    }
+    return res.status(200).json(treinos);
+  } catch (error) {
+    console.error("Erro ao buscar treinos:", error); // colocar logger messages
+    return res.status(500).json({ error: "Erro ao buscar treinos" }); // colocar logger messages
   }
 };
 
-module.exports = treinoController;
+module.exports = {
+  listarTreinos,
+};
--- backend/router/treinoRoutes.js
+++ backend/router/treinoRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const treinoController = require("../controllers/treinoController");
-const { roleMiddleware}  = require("../middleware/auth");
+const { listarTreinos } = require("../controllers/treinoController");
+const { roleMiddleware } = require("../middleware/auth");
 
-router.get("/", roleMiddleware, treinoController.listar);
+router.get("/", roleMiddleware, listarTreinos);
 
 module.exports = router;
