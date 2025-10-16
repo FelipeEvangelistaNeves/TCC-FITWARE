f995d85fcb61921c2e9d68f1a9a5e7564fdd408c
Anthony Lima Dias <azafcg2@gmail.com>
2025-10-16 18:24:07 -0300

    Começo de teste de swagger para api treinos

--- backend/router/treinoRoutes.js
+++ backend/router/treinoRoutes.js
@@ -4,6 +4,17 @@ const router = express.Router();
 const treinoController = require("../controllers/treinoController");
 const { roleMiddleware}  = require("../middleware/auth");
 
+/**
+ * @swagger
+ * /api/treinos:
+ * 
+ * 
+ *     responses:
+ *       200:
+ *         description: Fetch bem-sucedido
+ *       500:
+ *         description: Erro ao buscar treinos
+ */
 router.get("/", roleMiddleware, treinoController.listar);
 
 module.exports = router;
